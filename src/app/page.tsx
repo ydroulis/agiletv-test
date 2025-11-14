7556dfeb3e0bef05dc9288ea6530ceb49a48cd70
ydroulis <lombardi.droulis@gmail.com>
2025-11-14 10:32:48 -0300

    feat: home page restructuring

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,11 @@
-'use client'
 
-import { useAnimalStore } from '@/store/useAnimalStore'
-import { useRouter } from 'next/navigation'
-import { useEffect, useState } from 'react'
+import SearchSection from '@/components/SearchSection'
 
 export default function HomePage() {
-  const { setIsShowingResults } = useAnimalStore()
-
-  const router = useRouter()
-  const [term, setTerm] = useState('')
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
-    router.push(`/results?search=${term}`)
-  }
-
-  useEffect(() => {
-    setIsShowingResults(false)
-  }, [])
 
   return (
-    <main className="container" style={{ padding: 32 }}>
-      <header><h1>Animal Finder</h1></header>
-
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={term}
-          onChange={(e) => setTerm(e.target.value)}
-          placeholder="Buscar animais..."
-        />
-        <button type="submit">Buscar</button>
-      </form>
-
-      <footer><small>© 2025 Animal Finder</small></footer>
+    <main>
+      <SearchSection />
     </main>
   )
 }
